tiborvass/docker

refactor network inspect integration tests to use network package
Signed-off-by: Arash Deshmeh <adeshmeh@ca.ibm.com>

integration/network/inspect_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/tiborvass/docker/api/types/filters"
 	swarmtypes "github.com/tiborvass/docker/api/types/swarm"
 	"github.com/tiborvass/docker/client"
+	"github.com/tiborvass/docker/integration/internal/network"
 	"github.com/tiborvass/docker/integration/internal/swarm"
 	"gotest.tools/assert"
 	"gotest.tools/poll"
@@ -24,14 +25,10 @@ func TestInspectNetwork(t *testing.T) {
 	defer client.Close()
 
 	overlayName := "overlay1"
-	networkCreate := types.NetworkCreate{
-		CheckDuplicate: true,
-		Driver:         "overlay",
-	}
-
-	netResp, err := client.NetworkCreate(context.Background(), overlayName, networkCreate)
-	assert.NilError(t, err)
-	overlayID := netResp.ID
+	overlayID := network.CreateNoError(t, context.Background(), client, overlayName,
+		network.WithDriver("overlay"),
+		network.WithCheckDuplicate(),
+	)
 
 	var instances uint64 = 4
 	serviceName := "TestService" + t.Name()
@@ -44,7 +41,7 @@ func TestInspectNetwork(t *testing.T) {
 
 	poll.WaitOn(t, serviceRunningTasksCount(client, serviceID, instances), swarm.ServicePoll)
 
-	_, _, err = client.ServiceInspectWithRaw(context.Background(), serviceID, types.ServiceInspectOptions{})
+	_, _, err := client.ServiceInspectWithRaw(context.Background(), serviceID, types.ServiceInspectOptions{})
 	assert.NilError(t, err)
 
 	// Test inspect verbose with full NetworkID

ms-iot/moby

integration/network/inspect_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/docker/docker/api/types/filters"
 	swarmtypes "github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/integration/internal/network"
 	"github.com/docker/docker/integration/internal/swarm"
 	"gotest.tools/assert"
 	"gotest.tools/poll"
@@ -24,14 +25,10 @@ func TestInspectNetwork(t *testing.T) {
 	defer client.Close()
 
 	overlayName := "overlay1"
-	networkCreate := types.NetworkCreate{
-		CheckDuplicate: true,
-		Driver:         "overlay",
-	}
-
-	netResp, err := client.NetworkCreate(context.Background(), overlayName, networkCreate)
-	assert.NilError(t, err)
-	overlayID := netResp.ID
+	overlayID := network.CreateNoError(t, context.Background(), client, overlayName,
+		network.WithDriver("overlay"),
+		network.WithCheckDuplicate(),
+	)
 
 	var instances uint64 = 4
 	serviceName := "TestService" + t.Name()
@@ -44,7 +41,7 @@ func TestInspectNetwork(t *testing.T) {
 
 	poll.WaitOn(t, serviceRunningTasksCount(client, serviceID, instances), swarm.ServicePoll)
 
-	_, _, err = client.ServiceInspectWithRaw(context.Background(), serviceID, types.ServiceInspectOptions{})
+	_, _, err := client.ServiceInspectWithRaw(context.Background(), serviceID, types.ServiceInspectOptions{})
 	assert.NilError(t, err)
 
 	// Test inspect verbose with full NetworkID