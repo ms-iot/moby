tiborvass/docker

system: add back lcow validation function
Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

api/server/router/build/build_routes.go
@@ -24,6 +24,7 @@ import (
 	"github.com/tiborvass/docker/pkg/ioutils"
 	"github.com/tiborvass/docker/pkg/progress"
 	"github.com/tiborvass/docker/pkg/streamformatter"
+	"github.com/tiborvass/docker/pkg/system"
 	"github.com/docker/go-units"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -75,6 +76,9 @@ func newImageBuildOptions(ctx context.Context, r *http.Request) (*types.ImageBui
 		if apiPlatform != "" {
 			sp, err := platforms.Parse(apiPlatform)
 			if err != nil {
+				return nil, err
+			}
+			if err := system.ValidatePlatform(sp); err != nil {
 				return nil, err
 			}
 			options.Platform = &sp

api/server/router/image/image_routes.go
@@ -16,6 +16,7 @@ import (
 	"github.com/tiborvass/docker/errdefs"
 	"github.com/tiborvass/docker/pkg/ioutils"
 	"github.com/tiborvass/docker/pkg/streamformatter"
+	"github.com/tiborvass/docker/pkg/system"
 	"github.com/tiborvass/docker/registry"
 	specs "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
@@ -47,6 +48,9 @@ func (s *imageRouter) postImagesCreate(ctx context.Context, w http.ResponseWrite
 		if apiPlatform != "" {
 			sp, err := platforms.Parse(apiPlatform)
 			if err != nil {
+				return err
+			}
+			if err := system.ValidatePlatform(sp); err != nil {
 				return err
 			}
 			platform = &sp

ms-iot/moby

api/server/router/build/build_routes.go
@@ -24,6 +24,7 @@ import (
 	"github.com/docker/docker/pkg/ioutils"
 	"github.com/docker/docker/pkg/progress"
 	"github.com/docker/docker/pkg/streamformatter"
+	"github.com/docker/docker/pkg/system"
 	"github.com/docker/go-units"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -75,6 +76,9 @@ func newImageBuildOptions(ctx context.Context, r *http.Request) (*types.ImageBui
 		if apiPlatform != "" {
 			sp, err := platforms.Parse(apiPlatform)
 			if err != nil {
+				return nil, err
+			}
+			if err := system.ValidatePlatform(sp); err != nil {
 				return nil, err
 			}
 			options.Platform = &sp

api/server/router/image/image_routes.go
@@ -16,6 +16,7 @@ import (
 	"github.com/docker/docker/errdefs"
 	"github.com/docker/docker/pkg/ioutils"
 	"github.com/docker/docker/pkg/streamformatter"
+	"github.com/docker/docker/pkg/system"
 	"github.com/docker/docker/registry"
 	specs "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
@@ -47,6 +48,9 @@ func (s *imageRouter) postImagesCreate(ctx context.Context, w http.ResponseWrite
 		if apiPlatform != "" {
 			sp, err := platforms.Parse(apiPlatform)
 			if err != nil {
+				return err
+			}
+			if err := system.ValidatePlatform(sp); err != nil {
 				return err
 			}
 			platform = &sp